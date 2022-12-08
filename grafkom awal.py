ec040e990d277429717d43acf0492e3160533d49
nillaaayu <212410103050@mail.unej.ac.id>
2022-12-08 22:25:40 +0700

    revisi speed, collision

--- grafkom awal.py
+++ grafkom awal.py
@@ -38,8 +38,8 @@ for i in range(-350, 350, 10):
 ukuran_meteor = [10, 30, 40]
 
 # kecepatan meteor
-speed_meteor = 0.2
-speed_meteor1 = 0.2
+speed_meteor = 0.3
+speed_meteor1 = 0.3
 
 # spawn random meteor
 meteor = [random_X_spawn[rn.randint(0, len(random_X_spawn) - 1 )], ukuran_meteor[rn.randint(0, 2)]]
@@ -90,21 +90,6 @@ def gerak_meteor():
             time.sleep(1)
             tabrak = True
 
-    # score = int(score)
-    # score += 1
-    # if (score%100) == 1:
-    #     speed_meteor += 0.0001
-    #     score = str(score)
-    # else : 
-    #     score = str(score)   
-
-    # lvl = int(lvl) 
-    # lvl += 1
-    # if (lvl%100) == 1:
-    #     speed_meteor += 0.0001
-    #     lvl = str(lvl)
-    # else : 
-    #     lvl = str(lvl)
 
 def gerak_meteor1():
     global Y_meteor, meteor, speed_meteor, x_jalan_kiri, x_jalan_kanan, x_batas_kiri_player, x_batas_kanan_player, score, lvl, tabrak
@@ -126,22 +111,6 @@ def gerak_meteor1():
         if (gerak_x in range(meteor[0] - 60, meteor[0] + 70)) <= -1140:
             time.sleep(1)
             tabrak = True
-
-    # score = int(score)
-    # score += 1
-    # if (score%100) == 1:
-    #     speed_meteor += 0.001
-    #     score = str(score)
-    # else : 
-    #     score = str(score)
-
-    # lvl = int(lvl) 
-    # lvl += 1
-    # if (lvl%100) == 1:
-    #     speed_meteor += 0.001
-    #     lvl = str(lvl)
-    # else : 
-    #     lvl = str(lvl)
 
 def meteorsPoints(x, y, s):
     glTranslate(0, Y_meteor, 0)
@@ -218,16 +187,11 @@ def scoring():
         score += 1
         if (score%10000) == 1:
             lvl += 1
-<<<<<<< HEAD
-            speed_meteor += 0.5
-=======
             speed_meteor += 0.05
->>>>>>> e7ec994c2844aaa7ad69aef07af63a435917271c
             speed_meteor1 += 0.05
 
 def circle(r,xR,yR):
     glPushMatrix()
-    glColor3ub(0,0,255)
     glBegin(GL_POLYGON)
     for i in range(360):
         theta= 2 *3.1415926*i/360
@@ -238,8 +202,21 @@ def circle(r,xR,yR):
     glPopMatrix()
 
 def planet():
-    global plnt_x, plnt_y
-    circle(10,20,20)
+    # planet 1
+    glColor3ub(190,130,20)
+    circle(100,200,500)
+
+    # planet 2
+    glColor3ub(0,105,205)
+    circle(180,-200,300)
+
+    # planet 3
+    glColor3ub(205,0,0)
+    circle(260,-600,-100)
+
+    # planet 4
+    glColor3ub(255,255,0)
+    circle(400,700,-100)
 
 def player():
     global gerak_x, gerak_y
@@ -258,8 +235,8 @@ def player():
     # LEFT WING
     glBegin(GL_QUADS)
     glColor3ub(150, 0, 0)
-    glVertex2f(pla_x_1 - 5 + gerak_x, pla_y_1 - 60 + gerak_y)
-    glVertex2f(pla_x_1 - 5 + gerak_x, pla_y_1 - 40 + gerak_y)
+    glVertex2f(pla_x_1 - 7 + gerak_x, pla_y_1 - 66 + gerak_y)
+    glVertex2f(pla_x_1 - 7 + gerak_x, pla_y_1 - 47 + gerak_y)
     glVertex2f(pla_x_1 + 12 + gerak_x, pla_y_1 + 10 + gerak_y)
     glVertex2f(pla_x_1 + 5 + gerak_x, pla_y_1 - 40 + gerak_y)
     glEnd()
@@ -270,7 +247,7 @@ def player():
     glVertex2f(pla_x_1 + 68 + gerak_x, pla_y_1 + 10 + gerak_y)
     glVertex2f(pla_x_1 + 87 + gerak_x, pla_y_1 - 47 + gerak_y)
     glVertex2f(pla_x_1 + 87 + gerak_x, pla_y_1 - 65 + gerak_y)
-    glVertex2f(pla_x_1 + 72 + gerak_x, pla_y_1 - 40 + gerak_y)
+    glVertex2f(pla_x_1 + 75 + gerak_x, pla_y_1 - 40 + gerak_y)
     glEnd()
 
     # WINDOW
@@ -282,8 +259,27 @@ def player():
     glVertex2f(pla_x_1 + 28 + gerak_x, pla_y_1 + 8 + gerak_y)
     glEnd()
 
+    # NOS1
+    glBegin(GL_QUADS)
+    glColor3ub(75, 75, 75)
+    glVertex2f(pla_x_1 + 12 + gerak_x, pla_y_1 - 40 + gerak_y)
+    glVertex2f(pla_x_1 + 68 + gerak_x, pla_y_1 - 40 + gerak_y)
+    glVertex2f(pla_x_1 + 68 + gerak_x, pla_y_1 - 56 + gerak_y)
+    glVertex2f(pla_x_1 + 12 + gerak_x, pla_y_1 - 56 + gerak_y)
+    glEnd()
+
+    # NOS2
+    glBegin(GL_QUADS)
+    glColor3ub(255, 85, 0)
+    glVertex2f(pla_x_1 + 48 + gerak_x, pla_y_1 - 68 + gerak_y)
+    glVertex2f(pla_x_1 + 32 + gerak_x, pla_y_1 - 68 + gerak_y)
+    glVertex2f(pla_x_1 + 32 + gerak_x, pla_y_1 - 56 + gerak_y)
+    glVertex2f(pla_x_1 + 48 + gerak_x, pla_y_1 - 56 + gerak_y)
+    glEnd()
+
+
 def input_keyboard(key, x, y):
-    global gerak_x, gerak_y, x_batas_kiri_player, x_batas_kanan_player,play
+    global gerak_x, gerak_y, x_batas_kiri_player, x_batas_kanan_player, play, tabrak
 
     # Untuk mengubah posisi player
     if key == GLUT_KEY_RIGHT:
@@ -291,11 +287,11 @@ def input_keyboard(key, x, y):
     if key == GLUT_KEY_LEFT:
         gerak_x -= 20
     if key == GLUT_KEY_F1:
->>>>>>> e7ec994c2844aaa7ad69aef07af63a435917271c
             play = 1
 
-    if gerak_x == x_batas_kiri_player or gerak_x == x_batas_kanan_player:
-        glutLeaveMainLoop()
+    if gerak_x - 22 < x_batas_kiri_player or gerak_x + 50 > x_batas_kanan_player:
+        time.sleep(1)
+        tabrak = True
 
 def text_menu(x, y, font, text, r,  g , b , a):
     blending = False 
@@ -347,19 +343,11 @@ def menu():
             Level()
             glPopMatrix()
         else:
-<<<<<<< HEAD
-            time.sleep(3)
-=======
->>>>>>> e7ec994c2844aaa7ad69aef07af63a435917271c
             text_menu(-100,0, glut.GLUT_BITMAP_HELVETICA_18, "GAME OVER", 1, 1, 1, 0)
     else:
         text_menu(-130,100, glut.GLUT_BITMAP_TIMES_ROMAN_24, "Rocket Keeper", 1, 1, 1, 0)
         text_menu(-100,0, glut.GLUT_BITMAP_HELVETICA_18, "PLAY GAME", 1, 1, 1, 0)
-<<<<<<< HEAD
-        text_menu(-50,-50, glut.GLUT_BITMAP_HELVETICA_12, "PRESS UP", 1, 1, 1, 0)
-=======
         text_menu(-50,-50, glut.GLUT_BITMAP_HELVETICA_12, "PRESS F1", 1, 1, 1, 0)
->>>>>>> e7ec994c2844aaa7ad69aef07af63a435917271c
         # kalo tombol play diklik, masuk ke game
     glFlush()
 
